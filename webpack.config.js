44ea4a6243bfd7eb62e3d1ac14167269d653cb2b
John Nguyen <jnguyenc@gmail.com>
2022-10-01 20:14:14 -0400

    change build folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,57 +4,55 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 module.exports = (env, argv) => {
-  let mode = 'development';
-  if (argv.mode === 'production') {
-    mode = 'production';
-  }
+    let mode = 'development';
+    if (argv.mode === 'production') {
+        mode = 'production';
+    }
 
-  const config = {
-    entry: './src/js/index.js',
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: '[name].[contenthash].css',
-      }),
-      new HtmlWebpackPlugin({
-        title: 'Custom template',
-        template: path.resolve(__dirname, './src/index.html'),
-      }),
-      new WebpackManifestPlugin(),
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: ['babel-loader'],
+    const config = {
+        entry: './src/js/index.js',
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: '[name].[contenthash].css',
+            }),
+            new HtmlWebpackPlugin({
+                title: 'Custom template',
+                template: path.resolve(__dirname, './src/index.html'),
+            }),
+            new WebpackManifestPlugin(),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader'],
+                },
+                {
+                    test: /\.s?css$/,
+                    use: [
+                        mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader',
+                    ],
+                },
+            ],
         },
-        {
-          test: /\.s?css$/,
-          use: [
-            mode === 'development'
-              ? 'style-loader'
-              : MiniCssExtractPlugin.loader,
-            'css-loader',
-            'sass-loader',
-          ],
+        output: {
+            filename: '[name].[contenthash].bundle.js',
+            path: path.resolve(__dirname, mode === 'development' ? 'dist/dev' : 'public'),
+            clean: true,
         },
-      ],
-    },
-    output: {
-      filename: '[name].[contenthash].bundle.js',
-      path: path.resolve(__dirname, mode === 'development' ? 'dist/dev' : 'dist/prod'),
-      clean: true,
-    },
-    devtool: mode === 'development' ? 'source-map' : false,
-    devServer: {
-      static: {
-        directory: path.resolve(__dirname, 'dist/server'),
-      },
-      port: 9090,
-      compress: true,
-      hot: true,
-    },
-  };
+        devtool: mode === 'development' ? 'source-map' : false,
+        devServer: {
+            static: {
+                directory: path.resolve(__dirname, 'dist/server'),
+            },
+            port: 9090,
+            compress: true,
+            hot: true,
+        },
+    };
 
-  return config;
+    return config;
 };
