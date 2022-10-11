8784b0430fda67f683703b34868122bae49d8e84
John Nguyen <n1nguyen@bop.gov>
2022-10-11 15:52:37 -0400

    Added readme, adjusted dataURLs

--- src/js/dataUrls.js
+++ src/js/dataUrls.js
@@ -1,5 +1,5 @@
-/*
-const dataUrlsOri = [
+// This is for production
+const dataUrlsProd = [
   'data/rrc.json',
   'data/locations.json',
   'data/additional.json',
@@ -8,9 +8,9 @@ const dataUrlsOri = [
   '/coronavirus/json/vaccineInfo.json',
   '/PublicInfo/execute/opslevel?todo=query&output=json',
 ];
-*/
 
-const dataUrls = [
+// This is for development
+const dataUrlsDev = [
   'data/rrc.json',
   'data/locations.json',
   'data/additional.json',
@@ -19,5 +19,8 @@ const dataUrls = [
   'data/vaccineInfo.json',
   'data/opslevel.json',
 ];
+
+// Change this line depend on the build for production or development
+const dataUrls = dataUrlsDev;
 
 export default dataUrls;
