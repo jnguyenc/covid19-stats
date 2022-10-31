cd0efac7500c69e5ad65cfaee63ada780149773e
John Nguyen <n1nguyen@bop.gov>
2022-10-30 20:44:33 -0400

    fixed whitespaces only

--- src/js/fixData.js
+++ src/js/fixData.js
@@ -1,70 +1,70 @@
 import allCorrections from './dataCorrections';
 
 function editDistance(str1, str2) {
-    const s1 = str1.toLowerCase();
-    const s2 = str2.toLowerCase();
+  const s1 = str1.toLowerCase();
+  const s2 = str2.toLowerCase();
 
-    const costs = [];
-    for (let i = 0; i <= s1.length; i += 1) {
-        let lastValue = i;
-        for (let j = 0; j <= s2.length; j += 1) {
-            if (i === 0) {
-                costs[j] = j;
-            } else if (j > 0) {
-                let newValue = costs[j - 1];
-                if (s1.charAt(i - 1) !== s2.charAt(j - 1)) {
-                    newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
-                }
-                costs[j - 1] = lastValue;
-                lastValue = newValue;
-            }
+  const costs = [];
+  for (let i = 0; i <= s1.length; i += 1) {
+    let lastValue = i;
+    for (let j = 0; j <= s2.length; j += 1) {
+      if (i === 0) {
+        costs[j] = j;
+      } else if (j > 0) {
+        let newValue = costs[j - 1];
+        if (s1.charAt(i - 1) !== s2.charAt(j - 1)) {
+          newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
         }
-        if (i > 0) {
-            costs[s2.length] = lastValue;
-        }
+        costs[j - 1] = lastValue;
+        lastValue = newValue;
+      }
     }
-    return costs[s2.length];
+    if (i > 0) {
+      costs[s2.length] = lastValue;
+    }
+  }
+  return costs[s2.length];
 }
 
 function similarity(s1, s2) {
-    let longer = s1;
-    let shorter = s2;
-    if (s1.length < s2.length) {
-        longer = s2;
-        shorter = s1;
-    }
-    const longerLength = longer.length;
-    if (longerLength === 0) {
-        return 1.0;
-    }
-    return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+  let longer = s1;
+  let shorter = s2;
+  if (s1.length < s2.length) {
+    longer = s2;
+    shorter = s1;
+  }
+  const longerLength = longer.length;
+  if (longerLength === 0) {
+    return 1.0;
+  }
+  return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
 }
 
 function fixField(data, key = 'id', threshold = 0.75) {
-    const corrections = allCorrections[key];
+  const corrections = allCorrections[key];
 
-    corrections.forEach((correction) => {
-        data.forEach((r) => {
-            const alike = similarity(correction, r[key]);
-            if (alike > threshold) {
-                console.log(correction, 'vs.', r[key], ':', alike);
-                // eslint-disable-next-line no-param-reassign
-                r[key] = correction;
-            }
-        });
+  corrections.forEach((correction) => {
+    data.forEach((r) => {
+      const alike = similarity(correction, r[key]);
+      if (alike > threshold) {
+        console.log(correction, 'vs.', r[key], ':', alike);
+        // eslint-disable-next-line no-param-reassign
+        r[key] = correction;
+      }
     });
+  });
 }
 
 function copyField(data, from = 'code', to = 'facilityCode', consoleOut = false) {
-    data.forEach((d) => {
-        if (d[to] === undefined && d[from] !== undefined) {
-            // eslint-disable-next-line no-param-reassign
-            d[to] = d[from];
-            if (consoleOut) {
-                console.log('copied', d[from]);
-            }
-        }
-    });
+  data.forEach((d) => {
+    if (d[to] === undefined && d[from] !== undefined) {
+      // eslint-disable-next-line no-param-reassign
+      d[to] = d[from];
+      if (consoleOut) {
+        console.log('copied', d[from]);
+      }
+    }
+  });
 }
 
 export { fixField, copyField };
--- src/js/index.js
+++ src/js/index.js
@@ -8,58 +8,58 @@ import queryProcess from './queryProcess';
 import { copyField, fixField } from './fixData';
 
 fetchData(dataUrls)
-    .then((allJson) => {
-        const { RRC } = allJson[0];
-        const { Locations } = allJson[1];
-        const { facls } = allJson[2];
-        const { bopData, rrcData, privateData } = allJson[3];
-        const { bopTesting, rrcTesting } = allJson[4];
-        const { bopVaccine } = allJson[5];
-        const { OpsLevel } = allJson[6];
+  .then((allJson) => {
+    const { RRC } = allJson[0];
+    const { Locations } = allJson[1];
+    const { facls } = allJson[2];
+    const { bopData, rrcData, privateData } = allJson[3];
+    const { bopTesting, rrcTesting } = allJson[4];
+    const { bopVaccine } = allJson[5];
+    const { OpsLevel } = allJson[6];
 
-        let bopLocations = [];
-        let rrcLocations = [];
-        let allLocations = [];
+    let bopLocations = [];
+    let rrcLocations = [];
+    let allLocations = [];
 
-        bopLocations = mergeData(bopData, 'id', Locations, 'code');
-        bopLocations = mergeData(bopLocations, 'code', OpsLevel, 'faclCode');
+    bopLocations = mergeData(bopData, 'id', Locations, 'code');
+    bopLocations = mergeData(bopLocations, 'code', OpsLevel, 'faclCode');
 
-        // copy OpsLevel from complex to facilities
-        bopLocations = mergeData(bopLocations, 'complexCode', OpsLevel, 'faclCode');
+    // copy OpsLevel from complex to facilities
+    bopLocations = mergeData(bopLocations, 'complexCode', OpsLevel, 'faclCode');
 
-        bopLocations = mergeData(bopLocations, 'code', bopVaccine, 'facility');
+    bopLocations = mergeData(bopLocations, 'code', bopVaccine, 'facility');
 
-        // copy staffCompleted and inmateCompleted from complex to facilities - NOT CORRECT
-        // bopLocations = mergeData(bopLocations, 'complexCode', bopVaccine, 'facility');
+    // copy staffCompleted and inmateCompleted from complex to facilities - NOT CORRECT
+    // bopLocations = mergeData(bopLocations, 'complexCode', bopVaccine, 'facility');
 
-        bopLocations = mergeData(bopLocations, 'code', bopTesting, 'facilityCode');
-        // writeTable(bopLocations, dataColumns, '#area1');
+    bopLocations = mergeData(bopLocations, 'code', bopTesting, 'facilityCode');
+    // writeTable(bopLocations, dataColumns, '#area1');
 
-        rrcLocations = mergeData(rrcData, 'contractNum', RRC, 'contractNum');
-        rrcLocations = mergeData(rrcLocations, 'contractNum', facls, 'contractNum');
-        rrcLocations = mergeData(rrcLocations, 'id', rrcTesting, 'facilityName');
-        // rrcLocations = mergeData(rrcLocations, 'facilityCode', OpsLevel, 'faclCode');
-        // rrcLocations = mergeData(rrcLocations, 'facilityCode', bopVaccine, 'facility');
-        // writeTable(rrcLocations, dataColumns);
+    rrcLocations = mergeData(rrcData, 'contractNum', RRC, 'contractNum');
+    rrcLocations = mergeData(rrcLocations, 'contractNum', facls, 'contractNum');
+    rrcLocations = mergeData(rrcLocations, 'id', rrcTesting, 'facilityName');
+    // rrcLocations = mergeData(rrcLocations, 'facilityCode', OpsLevel, 'faclCode');
+    // rrcLocations = mergeData(rrcLocations, 'facilityCode', bopVaccine, 'facility');
+    // writeTable(rrcLocations, dataColumns);
 
-        // combine locations
-        allLocations = [...bopLocations, ...rrcLocations];
+    // combine locations
+    allLocations = [...bopLocations, ...rrcLocations];
 
-        // fix the data - copy code to facilityCode
-        copyField(allLocations, 'code', 'facilityCode');
+    // fix the data - copy code to facilityCode
+    copyField(allLocations, 'code', 'facilityCode');
 
-        // fix the data - copy nameDisplay to name
-        copyField(allLocations, 'name', 'nameDisplay');
+    // fix the data - copy nameDisplay to name
+    copyField(allLocations, 'name', 'nameDisplay');
 
-        writeTable(allLocations, dataColumns, '#area1');
+    writeTable(allLocations, dataColumns, '#area1');
 
-        queryProcess();
+    queryProcess();
 
-        // what to do with this privateDate?
-        console.log(privateData);
+    // what to do with this privateDate?
+    console.log(privateData);
 
-        // End of .then
-    })
-    .catch((err) => {
-        console.error('There was error', err);
-    });
+    // End of .then
+  })
+  .catch((err) => {
+    console.error('There was error', err);
+  });
